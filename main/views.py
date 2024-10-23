8d1f523e68047cf2cc26415398412bdb4219636a
Marcelo Cortez <mhcortez@gmail.com>
2024-10-23 16:43:17 -0300

    finalização
    alteraçoes

--- main/views.py
+++ main/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render, redirect
 from .models import Task
+from django.shortcuts import get_object_or_404
 
 def task_list(request):
     tasks =Task.objects.all()
-    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+    return render(request, 'main/task_list.html', {'tasks': tasks})
 
 def add_task(request):
-    if request.method == 'POST'
+    if request.method == 'POST':
         title = request.POST.get('title')
         if title:
             Task.objects.create(title=title)
         return redirect('task_list')
-    return render(request, 'tasks/add_task.html')
+    return render(request, 'main/add_task.html')
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
+
+def edit_task(request, task_id):
+    task = get_object_or_404(task,id=task_id)
+    
+    if request.method == 'POST':
+        task.title = request.POST.get('title')
+        task.complete = 'complete' in  request.POST #atualiza o ststus com base no checkbox
+        task.save()
+        return redirect('task_list')
+        
+    return render(request, 'main/edit_task.html', {'task': task})
